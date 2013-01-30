e6293c5797ef6ae33399ad33f472dd2cc92e0e80
Kyle <Kyle@Kyle-Laptop>
2013-01-29 17:40:46 -0700

    Im am simply amazing :p

---
+++ AnTutorial/src/com/tutorial/kyle/Splash.java
@@ -0,0 +1,28 @@
+package com.tutorial.kyle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent openStartingPoint = new Intent("com.tutorial.kyle.MENU");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
+	}
+}
