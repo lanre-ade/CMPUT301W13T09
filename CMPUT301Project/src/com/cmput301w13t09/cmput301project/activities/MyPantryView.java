b2124b932a643cc559c1830a336fedbb5f836c9f
Marcus <mkarpoff@ualberta.ca>
2013-03-08 02:49:13 -0700

    FIXED THE MOST RETARDED BUG IN THE WORLD.... EVERYONE TELL FRANK HUANG THAT YOU LOVE HIM BECAUSE WE WOULD BE VERY VERY SCREWED WITHOUT THE ASSISTANCE HE PROVIDED Coding time: Hour X12 Caffeine consumed: Red Rain X3

--- CMPUT301Project/src/com/cmput301w13t09/cmput301project/activities/MyPantryView.java
+++ CMPUT301Project/src/com/cmput301w13t09/cmput301project/activities/MyPantryView.java
@@ -3,6 +3,8 @@ package com.cmput301w13t09.cmput301project.activities;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView.OnItemClickListener;
@@ -16,13 +18,10 @@ import com.cmput301w13t09.cmput301project.IngredientModel;
 import com.cmput301w13t09.cmput301project.R;
 
 public class MyPantryView extends Activity {
-	
-	private static final int ITEM_CLICK = 0, ADD_INGREDIENT = 1;
-	
+
 	private ListAdapter ingredientListAdapter;
 	private ListView ingredientListView;
 	private int dialogNumber;
-
 	private IngredientListModel ingredientList;
 
 	@Override
@@ -44,25 +43,56 @@ public class MyPantryView extends Activity {
 		ingredientListView.setAdapter(ingredientListAdapter);
 
 		ingredientListView.setOnItemClickListener(new OnItemClickListener() {
-			@SuppressWarnings("deprecation")
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				
+				dialogNumber = position;
+				AlertDialog.Builder builder = new AlertDialog.Builder(MyPantryView.this);
+				String title = ingredientList.getIngredientListName(position);
+				String message = ingredientList.getIngredient(position).toDialogString();
+				builder.setMessage(message);
+				builder.setTitle(title);
 
+				builder.setNegativeButton("Cancle",
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.dismiss();
+
+							}
+						});
+				builder.setNeutralButton("Edit",
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.dismiss();
+
+							}
+						});
+				builder.setPositiveButton("Delete",
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								ingredientList.remove(dialogNumber);
+								dialog.dismiss();
+								updateList();
+
+							}
+						});
+
+				AlertDialog dialog = builder.create();
+				dialog.show();
 			}
 		});
 
 	}
-	protected Dialog onCreateDialog(int id) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		switch(id) {
-		
-		case ITEM_CLICK:
-			return null;
-			
-		case ADD_INGREDIENT:
-			return null;
-		}
-		return null;
+
+	protected void updateList() {
+		ingredientListAdapter = new ArrayAdapter<IngredientModel>(this,
+				android.R.layout.simple_list_item_1,
+				ingredientList.getIngredientList());
+		ingredientListView.setAdapter(ingredientListAdapter);
 	}
 }
