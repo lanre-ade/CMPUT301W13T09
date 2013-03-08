ff831ca08ee87dbd0240523900176eb29314352d
Marcus <mkarpoff@ualberta.ca>
2013-03-08 16:49:18 -0700

    Started the 'Add New Ingredients' view

--- CMPUT301Project/gen/com/cmput301w13t09/cmput301project/R.java
+++ CMPUT301Project/gen/com/cmput301w13t09/cmput301project/R.java
@@ -33,19 +33,22 @@ public final class R {
         public static final int searchEditText=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main_menu_view=0x7f030000;
-        public static final int activity_my_pantry_view=0x7f030001;
-        public static final int activity_my_recipes_view=0x7f030002;
-        public static final int recipe_list_row=0x7f030003;
+        public static final int activity_add_new_ingredient=0x7f030000;
+        public static final int activity_main_menu_view=0x7f030001;
+        public static final int activity_my_pantry_view=0x7f030002;
+        public static final int activity_my_recipes_view=0x7f030003;
+        public static final int recipe_list_row=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main_menu_view=0x7f070000;
-        public static final int activity_my_pantry_view=0x7f070001;
-        public static final int activity_my_recipes_view=0x7f070002;
+        public static final int activity_add_new_ingredient=0x7f070000;
+        public static final int activity_main_menu_view=0x7f070001;
+        public static final int activity_my_pantry_view=0x7f070002;
+        public static final int activity_my_recipes_view=0x7f070003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int create_recipe=0x7f05000a;
+        public static final int hello_world=0x7f05000b;
         public static final int in_pantry_search_check=0x7f050003;
         public static final int local_search_text=0x7f050004;
         public static final int menu_settings=0x7f050001;
@@ -53,6 +56,7 @@ public final class R {
         public static final int pantry=0x7f050006;
         public static final int recipes=0x7f050005;
         public static final int search_text=0x7f050002;
+        public static final int title_activity_add_new_ingredient=0x7f05000c;
         public static final int title_activity_my_pantry_view=0x7f050007;
         public static final int title_activity_my_recipes_view=0x7f050008;
     }
---
+++ CMPUT301Project/src/com/cmput301w13t09/cmput301project/AddNewIngredient.java
@@ -0,0 +1,22 @@
+package com.cmput301w13t09.cmput301project;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AddNewIngredient extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_new_ingredient);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_add_new_ingredient, menu);
+		return true;
+	}
+
+}
