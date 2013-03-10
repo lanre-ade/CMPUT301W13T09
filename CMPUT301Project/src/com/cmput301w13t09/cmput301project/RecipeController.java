d4122b59098b3a453ae7543903630ce4cf0187bc
Marcus <mkarpoff@ualberta.ca>
2013-03-09 21:49:20 -0700

    Made Recipe System MVC

--- CMPUT301Project/src/com/cmput301w13t09/cmput301project/RecipeController.java
+++ CMPUT301Project/src/com/cmput301w13t09/cmput301project/RecipeController.java
@@ -1,6 +1,79 @@
 package com.cmput301w13t09.cmput301project;
 
-public class RecipeController {
-	
+import java.util.ArrayList;
 
+/**
+ * Class: RecipeListModel RecipeList is a class that stores a list of recipes.
+ * These recipes are from RecipeModel and are stored in ArrayList class. The
+ * constructor takes in a single recipe (RecipeModel)and appends it to a blank
+ * recipe list. RecipeLists methods are getLength, getRecipeListName,
+ * getRecipeListDesc7 getRecipeingredientList, and getRecipePhotoList.
+ */
+
+public class RecipeController {
+	private ArrayList<RecipeModel> recipe_list;
+
+	/**
+	 * Constructor 
+	 * 
+	 */
+	public RecipeController() {
+		this.recipe_list = new ArrayList<RecipeModel>();
+	}
+
+	/**
+	 * Returns length of recipe list (RecipeListModel)
+	 * 
+	 * @return size : The size of the list of recipes
+	 */
+	public int getLength() {
+		return recipe_list.size();
+	}
+
+	/**
+	 * Returns name of recipe (RecipeListModel) based on its position in list
+	 * 
+	 * @param i
+	 *            : Position of recipe in the list
+	 * @return Name of recipe
+	 */
+	public String getRecipeListName(int i) {
+		return recipe_list.get(i).getRecipeName();
+	}
+
+	/**
+	 * Returns description of recipe (RecipeListModel) based on its position in
+	 * list
+	 * 
+	 * @param i
+	 *            : Position of recipe in the list
+	 * @return Description of recipe
+	 */
+	public String getRecipeListDesc(int i) {
+		return recipe_list.get(i).getRecipeDesc();
+	}
+
+	/**
+	 * Returns list of ingredients of recipe (RecipeListModel) based on its
+	 * position in recipe list
+	 * 
+	 * @param i
+	 *            : Position of recipe in the list
+	 * @return List of ingredient that go into this recipe
+	 */
+	public IngredientListModel getRecipeingredientList(int i) {
+		return recipe_list.get(i).getIngredList();
+	}
+
+	/**
+	 * Returns list of photos of recipe (RecipeListModel) based on its position
+	 * in recipe list
+	 * 
+	 * @param i
+	 *            : Position of recipe in the list
+	 * @return List of photos attached to a recipe item
+	 */
+	public PhotoListModel getRecipePhotoList(int i) {
+		return recipe_list.get(i).getPhotoList();
+	}
 }
--- CMPUT301Project/src/com/cmput301w13t09/cmput301project/activities/MyRecipesView.java
+++ CMPUT301Project/src/com/cmput301w13t09/cmput301project/activities/MyRecipesView.java
@@ -2,102 +2,18 @@ package com.cmput301w13t09.cmput301project.activities;
 
 
 import android.app.Activity;
-import android.app.LoaderManager;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.content.CursorLoader;
-import android.content.Intent;
 
 import com.cmput301w13t09.cmput301project.R;
-import com.cmput301w13t09.cmput301project.RecipeDBAdapter;
 
 public class MyRecipesView extends Activity {
 
-	
-	private ListView recipeListView;
-	
-	private RecipeDBAdapter _dbHelper;
-	private Cursor _cursor;
-	private SimpleCursorAdapter adapter;
-	//private CursorLoader adaptr;
-	//private LoaderManager managr;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_my_recipes_view);
-		recipeListView = (ListView) findViewById(R.id.myRecipesList);
-		Button b = (Button) findViewById(R.id.createRecipeButton);
-		b.setOnClickListener(new OnClickListener() {
-
-		/*
-		public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						CreateRecipeView.class);
-				startActivity(intent);
-			}
-		*/
-			
-			@Override
-			public void onClick(View v) {
-				try {
-					Intent viewCreateRecipe = new Intent("activities.CreateRecipe");
-					startActivity(viewCreateRecipe);
-				} catch(Throwable e) {
-					e.printStackTrace();
-				}
-			}
-			
-		});
-		
-		_dbHelper = new RecipeDBAdapter(this);
 	}
-/*
-	protected void onStart() {
-		super.onStart();
-		
-		_dbHelper.open();
-		//fillData();
-	}
-
-	protected void onStop() {
-		super.onStop();
-		_dbHelper.close();
-		_cursor.close();
-	}
-*/
-	
-	/**
-	 * Fill the recipe list view with recipes
-	 */
-	@SuppressWarnings("deprecation")
-	private void fillData() {
-
-		_cursor = _dbHelper.fetchAllRecipes();
-		startManagingCursor(_cursor);
-
-		String[] from = new String[] { RecipeDBAdapter.RECIPE, RecipeDBAdapter.PROCEDURE };
-		int[] to = new int[] { R.id.recipe_name, R.id.recipe_desc};
-
-		//adapter = new SimpleCursorAdapter(this, R.layout.recipe_list_row, _cursor,
-			//	from, to);
-		
-		adapter = new SimpleCursorAdapter(this,
-	            R.layout.recipe_list_row, _cursor,
-	            from, to, 0);
-	    recipeListView.setAdapter(adapter);
-
-
-		stopManagingCursor(_cursor);
-
-	}
-
 }
 
