58f37f3a8cfa06fba656f4d534a49cf4a8a9d9cb
Lanre Adebambo <adebambo@ualberta.ca>
2013-03-11 16:05:41 -0600

    J Unit Testing. Still can't get tests to run

---
+++ CMPUT301Project/src/com/cmput301w13t09/cmput301project/tests/RecipeListModelTest.java
@@ -0,0 +1,17 @@
+package com.cmput301w13t09.cmput301project.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RecipeListModelTest {
+
+	public RecipeListModelTest() {
+	}
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
